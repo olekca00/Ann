afba1eec775e069e0a51f0be1c7469b994858004
olekca00 <olekca00@gmail.com>
2023-02-14 09:39:27 +0200

    Цикли в Java. Цикл for.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,35 @@
 public class Main {
-        public static void main(String[] args){
-            String s1 = "The quick brown fox jumps over the lazy dog."; // 1.
+    public static void main(String[] args) {
+//        for(int i = 0; i < 5 ; i++){
+//            System.out.println("i = " + i);
+//        }
 
-            int stringLength = s1.length(); //2.
-            System.out.println("length of s1 is " + stringLength); //3.
-            System.out.println(s1.replace('d', 'f')); //4.
+        double value = 238;
+        System.out.println("1% від " + value + " = " + calcPercent(value, 1));
+        System.out.println("2% від " + value + " = " + calcPercent(value, 2));
+        System.out.println("3% від " + value + " = " + calcPercent(value, 3));
+        System.out.println("4% від " + value + " = " + calcPercent(value, 4));
+        System.out.println("5% від " + value + " = " + calcPercent(value, 5));
+        System.out.println("6% від " + value + " = " + calcPercent(value, 6));
+        ////////
+        System.out.println("99% від " + value + " = " + calcPercent(value, 99));
+        System.out.println("100% від " + value + " = " + calcPercent(value, 100));
 
-            String s2 = "10-A. ";//5.
-            String s3 = "Bardysh Anna"; //6.
-            String s4 = s2.concat(s3); //7.
-            System.out.println(s4); //7.
+        for (int p = 0; p <= 100; p++) {
+            System.out.println(p + "% від " + value + " = " + calcPercent(value, p));
         }
+
+        //сума чисел від 1....n
+        int n = 999;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + i;
+        }
+        System.out.println("Сума числе від 0 до " + n + " буде " + sum);
+    }
+
+
+    public static double calcPercent(double v, int p) {
+        return v * p / 100;
+    }
 }
