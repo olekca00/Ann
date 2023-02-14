b0c6ebfe567ad94b64a11d2b0ad764ed109b42b2
olekca00 <olekca00@gmail.com>
2023-02-14 12:26:47 +0200

    Цикли в Java. Цикл for.

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-//        for(int i = 0; i < 5 ; i++){
-//            System.out.println("i = " + i);
-//        }
-
-        double value = 238;
-        System.out.println("1% від " + value + " = " + calcPercent(value, 1));
-        System.out.println("2% від " + value + " = " + calcPercent(value, 2));
-        System.out.println("3% від " + value + " = " + calcPercent(value, 3));
-        System.out.println("4% від " + value + " = " + calcPercent(value, 4));
-        System.out.println("5% від " + value + " = " + calcPercent(value, 5));
-        System.out.println("6% від " + value + " = " + calcPercent(value, 6));
-        ////////
-        System.out.println("99% від " + value + " = " + calcPercent(value, 99));
-        System.out.println("100% від " + value + " = " + calcPercent(value, 100));
-
-        for (int p = 0; p <= 100; p++) {
-            System.out.println(p + "% від " + value + " = " + calcPercent(value, p));
+        double value = 1;
+        System.out.println("Ряд чисел: 1 + 1/2 + 1/3 ... + 1/n; кроків - 100" );
+        //ряд чисел виду 1 + 1/2 + 1/3 ... + 1/x; кроків - 53
+        for (double n = 0; n < 53; n++) {
+            int i = 1;
+            i += n;
+            value += 1 / (n + 2);
+            System.out.println("Крок " + i + " = " + value);
         }
-
-        //сума чисел від 1....n
-        int n = 999;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum = sum + i;
-        }
-        System.out.println("Сума числе від 0 до " + n + " буде " + sum);
-    }
-
-
-    public static double calcPercent(double v, int p) {
-        return v * p / 100;
     }
 }
